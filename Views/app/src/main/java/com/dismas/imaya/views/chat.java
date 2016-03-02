2fa86a01cb7a2bd81e5732891d5ca30330ecdca8
ImayaDismas <imayadismas@gmail.com>
2016-03-02 03:56:32 +0300

    Add the activity back button

--- Views/app/src/main/java/com/dismas/imaya/views/chat.java
+++ Views/app/src/main/java/com/dismas/imaya/views/chat.java
@@ -1,7 +1,10 @@
 package com.dismas.imaya.views;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 
 /**
@@ -14,6 +17,26 @@ public class chat extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.chat);
+        addListenerOnButton();
+    }
+    public void addListenerOnButton() {
+
+        final Context context = this;
+
+        button = (Button) findViewById(R.id.button6);
+
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Intent intent = new Intent(context, backmenu.class);
+                startActivity(intent);
+
+            }
+
+        });
+
     }
 
 
--- Views/app/src/main/java/com/dismas/imaya/views/home.java
+++ Views/app/src/main/java/com/dismas/imaya/views/home.java
@@ -1,7 +1,10 @@
 package com.dismas.imaya.views;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 /**
  * Created by imaya on 3/1/16.
@@ -13,6 +16,26 @@ public class home extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home);
+        addListenerOnButton();
+    }
+    public void addListenerOnButton() {
+
+        final Context context = this;
+
+        button = (Button) findViewById(R.id.button5);
+
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Intent intent = new Intent(context, backmenu.class);
+                startActivity(intent);
+
+            }
+
+        });
+
     }
 
 
