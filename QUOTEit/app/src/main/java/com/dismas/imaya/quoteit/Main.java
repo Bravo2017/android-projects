656374d3f0aed58af4d7548c7d13cee2c461f9ee
ImayaDismas <imayadismas@gmail.com>
2016-03-07 02:42:32 +0300

    linked all the activities and layouts to the main

--- QUOTEit/app/src/main/java/com/dismas/imaya/quoteit/Main.java
+++ QUOTEit/app/src/main/java/com/dismas/imaya/quoteit/Main.java
@@ -7,10 +7,12 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class Main extends AppCompatActivity {
+    Button button;
 
     private TextView textView;
     private BroadcastReceiver receiver = new BroadcastReceiver() {
@@ -39,6 +41,12 @@ public class Main extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        addListenerOnButton1();
+        addListenerOnButton2();
+        addListenerOnButton3();
+        addListenerOnButton4();
+        addListenerOnButton5();
+        addListenerOnButton6();
         textView = (TextView) findViewById(R.id.status);
     }
     @Override
@@ -61,5 +69,120 @@ public class Main extends AppCompatActivity {
                 "http://www.relatably.com/q/img/funny-quotes-about-friends/funny-quotes-about-friends-tumblr-.jpg");
         startService(intent);
         textView.setText("Service started");
+    }
+
+    public void addListenerOnButton1() {
+
+        final Context context = this;
+
+        button = (Button) findViewById(R.id.button1);
+
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Intent intent = new Intent(context, Life.class);
+                startActivity(intent);
+
+            }
+
+        });
+
+    }
+    public void addListenerOnButton2() {
+
+        final Context context = this;
+
+        button = (Button) findViewById(R.id.button2);
+
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Intent intent = new Intent(context, School.class);
+                startActivity(intent);
+
+            }
+
+        });
+
+    }
+    public void addListenerOnButton3() {
+
+        final Context context = this;
+
+        button = (Button) findViewById(R.id.button3);
+
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Intent intent = new Intent(context, Friends.class);
+                startActivity(intent);
+
+            }
+
+        });
+
+    }
+    public void addListenerOnButton4() {
+
+        final Context context = this;
+
+        button = (Button) findViewById(R.id.button4);
+
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Intent intent = new Intent(context, Kids.class);
+                startActivity(intent);
+
+            }
+
+        });
+
+    }
+    public void addListenerOnButton5() {
+
+        final Context context = this;
+
+        button = (Button) findViewById(R.id.button5);
+
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Intent intent = new Intent(context, Sayings.class);
+                startActivity(intent);
+
+            }
+
+        });
+
+    }
+    public void addListenerOnButton6() {
+
+        final Context context = this;
+
+        button = (Button) findViewById(R.id.button6);
+
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Intent intent = new Intent(context, Love.class);
+                startActivity(intent);
+
+            }
+
+        });
+
     }
 }
