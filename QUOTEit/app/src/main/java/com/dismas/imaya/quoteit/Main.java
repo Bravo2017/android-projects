950cd7831c35c01034739d88858e7855ae6f9b3a
ImayaDismas <imayadismas@gmail.com>
2016-03-07 02:34:20 +0300

    first friend download complete

--- QUOTEit/app/src/main/java/com/dismas/imaya/quoteit/Main.java
+++ QUOTEit/app/src/main/java/com/dismas/imaya/quoteit/Main.java
@@ -1,13 +1,65 @@
 package com.dismas.imaya.quoteit;
 
+import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Main extends AppCompatActivity {
+
+    private TextView textView;
+    private BroadcastReceiver receiver = new BroadcastReceiver() {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Bundle bundle = intent.getExtras();
+            if (bundle != null) {
+                String string = bundle.getString(DownloadService.FILEPATH);
+                int resultCode = bundle.getInt(DownloadService.RESULT);
+                if (resultCode == RESULT_OK) {
+                    Toast.makeText(Main.this,
+                            "Download complete. Download URI: " + string,
+                            Toast.LENGTH_LONG).show();
+                    textView.setText("Download done");
+                } else {
+                    Toast.makeText(Main.this, "Download failed",
+                            Toast.LENGTH_LONG).show();
+                    textView.setText("Download failed");
+                }
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        textView = (TextView) findViewById(R.id.status);
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        registerReceiver(receiver, new IntentFilter(DownloadService.NOTIFICATION));
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(receiver);
+    }
+
+    public void onClick(View view) {
+
+        Intent intent = new Intent(this, DownloadService.class);
+        // add infos for the service which file to download and where to store
+        intent.putExtra(DownloadService.FILENAME, "funny-quotes-about-friends-tumblr-.jpg");
+        intent.putExtra(DownloadService.URL,
+                "http://www.relatably.com/q/img/funny-quotes-about-friends/funny-quotes-about-friends-tumblr-.jpg");
+        startService(intent);
+        textView.setText("Service started");
     }
 }
