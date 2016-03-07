45efcff7a854a5770844a7bb55bdcbbac5b1a76f
ImayaDismas <imayadismas@gmail.com>
2016-03-07 03:48:41 +0300

    added the pictures and content

--- QUOTEit/app/src/main/java/com/dismas/imaya/quoteit/Main.java
+++ QUOTEit/app/src/main/java/com/dismas/imaya/quoteit/Main.java
@@ -14,29 +14,6 @@ import android.widget.Toast;
 public class Main extends AppCompatActivity {
     Button button;
 
-    private TextView textView;
-    private BroadcastReceiver receiver = new BroadcastReceiver() {
-
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            Bundle bundle = intent.getExtras();
-            if (bundle != null) {
-                String string = bundle.getString(DownloadService.FILEPATH);
-                int resultCode = bundle.getInt(DownloadService.RESULT);
-                if (resultCode == RESULT_OK) {
-                    Toast.makeText(Main.this,
-                            "Download complete. Download URI: " + string,
-                            Toast.LENGTH_LONG).show();
-                    textView.setText("Download done");
-                } else {
-                    Toast.makeText(Main.this, "Download failed",
-                            Toast.LENGTH_LONG).show();
-                    textView.setText("Download failed");
-                }
-            }
-        }
-    };
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -47,30 +24,7 @@ public class Main extends AppCompatActivity {
         addListenerOnButton4();
         addListenerOnButton5();
         addListenerOnButton6();
-        textView = (TextView) findViewById(R.id.status);
     }
-    @Override
-    protected void onResume() {
-        super.onResume();
-        registerReceiver(receiver, new IntentFilter(DownloadService.NOTIFICATION));
-    }
-    @Override
-    protected void onPause() {
-        super.onPause();
-        unregisterReceiver(receiver);
-    }
-
-    public void onClick(View view) {
-
-        Intent intent = new Intent(this, DownloadService.class);
-        // add infos for the service which file to download and where to store
-        intent.putExtra(DownloadService.FILENAME, "funny-quotes-about-friends-tumblr-.jpg");
-        intent.putExtra(DownloadService.URL,
-                "http://www.relatably.com/q/img/funny-quotes-about-friends/funny-quotes-about-friends-tumblr-.jpg");
-        startService(intent);
-        textView.setText("Service started");
-    }
-
     public void addListenerOnButton1() {
 
         final Context context = this;
