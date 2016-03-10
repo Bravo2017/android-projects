ffba4bbafd31549e8dfc94ea1e961ea67dd01768
ImayaDismas <imayadismas@gmail.com>
2016-03-10 05:28:17 +0300

    added the app logo

--- Smart-Login/app/src/main/java/com/dismas/imaya/smart_login/LoginActivity.java
+++ Smart-Login/app/src/main/java/com/dismas/imaya/smart_login/LoginActivity.java
@@ -1,13 +1,18 @@
 package com.dismas.imaya.smart_login;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.Toast;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 
+import studios.codelight.smartloginlibrary.SmartCustomLoginListener;
 import studios.codelight.smartloginlibrary.SmartLoginBuilder;
 import studios.codelight.smartloginlibrary.SmartLoginConfig;
 import studios.codelight.smartloginlibrary.users.SmartFacebookUser;
@@ -23,16 +28,50 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
         SmartLoginBuilder loginBuilder = new SmartLoginBuilder();
-        Intent intent = loginBuilder.with(LoginActivity.this)
-//                .setAppLogo(APP_LOGO)
-                .isFacebookLoginEnabled(true).withFacebookAppId("APP_ID")
-                .withFacebookPermissions((ArrayList<String>) Arrays.asList("public_profile"))
+
+        //Set facebook permissions
+        ArrayList<String> permissions = new ArrayList<>();
+        permissions.add("public_profile");
+        permissions.add("email");
+        permissions.add("user_birthday");
+        permissions.add("user_friends");
+
+//        use this or getApplicationContext()
+        Intent intent = loginBuilder.with(getApplicationContext())
+                .setAppLogo(R.drawable.coffeesplash)
+                .isFacebookLoginEnabled(true)
+                .withFacebookAppId(getString(R.string.facebook_app_id))
+                .withFacebookPermissions(permissions)
                 .isGoogleLoginEnabled(true)
+                .isCustomLoginEnabled(false).setSmartCustomLoginHelper(new SmartCustomLoginListener() {
+                    @Override
+                    public boolean customSignin(SmartUser user) {
+                        //This "user" will have only username and password set.
+                        Toast.makeText(LoginActivity.this, user.getUsername() + " " + user.getPassword(), Toast.LENGTH_SHORT).show();
+                        return true;
+                    }
+
+                    @Override
+                    public boolean customSignup(SmartUser newUser) {
+                        //Implement your our custom sign up logic and return true if success
+                        return true;
+                    }
+
+                    @Override
+                    public boolean customUserSignout(SmartUser smartUser) {
+                        //Implement logout logic
+                        return true;
+                    }
+
+
+                })
                 .build();
         startActivityForResult(intent, SmartLoginConfig.LOGIN_REQUEST);
         //setContentView(R.layout.main);
     }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         //Intent "data" contains the user object
@@ -58,5 +97,19 @@ public class LoginActivity extends AppCompatActivity {
         }else if(resultCode == RESULT_CANCELED){
             //Login Failed
         }
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // Logs 'install' and 'app activate' App Events.
+        AppEventsLogger.activateApp(this);
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        // Logs 'app deactivate' App Event.
+        AppEventsLogger.deactivateApp(this);
     }
 }
