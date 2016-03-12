3ee6ce13e2e89e7870214d5c68658695e22c8638
ImayaDismas <imayadismas@gmail.com>
2016-03-12 13:27:42 +0300

    Added all the files

--- MaterialScroll/app/src/main/java/com/dismas/imaya/materialscroll/Constants.java
+++ MaterialScroll/app/src/main/java/com/dismas/imaya/materialscroll/Constants.java
@@ -4,4 +4,19 @@ package com.dismas.imaya.materialscroll;
  * Created by imaya on 3/10/16.
  */
 public class Constants {
+    public static String[] ANDROID_VERSIONS = new String[]{
+            "Marshmallow",
+            "Lollipop",
+            "KitKat",
+            "Jelly Bean",
+            "Ice Cream Sandwich",
+            "Honeycomb",
+            "Gingerbread",
+            "Froyo",
+            "Eclair",
+            "Donut",
+            "Cupcake",
+            "Banana Bread",
+            "Apple Pie"
+    };
 }
