18495626e56057fac910bec88715af407f539e9b
ImayaDismas <imayadismas@gmail.com>
2016-03-12 19:13:22 +0300

    sets the toolbar’s image view and text view with the selected track’s data

--- SoundCPlayer/app/src/main/java/com/dismas/imaya/soundcplayer/Main.java
+++ SoundCPlayer/app/src/main/java/com/dismas/imaya/soundcplayer/Main.java
@@ -3,7 +3,13 @@ package com.dismas.imaya.soundcplayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -20,6 +26,8 @@ public class Main extends AppCompatActivity {
     private static final String TAG = "Main";
     private List<Track> mListItems;
     private SCTrackAdapter mAdapter;
+    private TextView mSelectedTrackTitle;
+    private ImageView mSelectedTrackImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +38,20 @@ public class Main extends AppCompatActivity {
         ListView listView = (ListView)findViewById(R.id.track_list_view);
         mAdapter = new SCTrackAdapter(this, mListItems);
         listView.setAdapter(mAdapter);
+
+        mSelectedTrackTitle = (TextView)findViewById(R.id.selected_track_title);
+        mSelectedTrackImage = (ImageView)findViewById(R.id.selected_track_image);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Track track = mListItems.get(position);
+
+                mSelectedTrackTitle.setText(track.getTitle());
+                Picasso.with(Main.this).load(track.getArtworkURL()).into(mSelectedTrackImage);
+
+            }
+        });
 
         SCService scService = SoundCloud.getService();
         scService.getRecentTracks(new SimpleDateFormat("yyyy-MM-dd hh:mm:ss").format(new Date()), new Callback<List<Track>>() {
