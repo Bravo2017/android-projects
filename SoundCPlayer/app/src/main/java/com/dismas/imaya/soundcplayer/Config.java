6df16f271b2c97e3790ee1b01d74d82cb5cc4f70
ImayaDismas <imayadismas@gmail.com>
2016-03-12 14:08:15 +0300

    Added a new activity

--- SoundCPlayer/app/src/main/java/com/dismas/imaya/soundcplayer/Config.java
+++ SoundCPlayer/app/src/main/java/com/dismas/imaya/soundcplayer/Config.java
@@ -4,4 +4,6 @@ package com.dismas.imaya.soundcplayer;
  * Created by imaya on 3/12/16.
  */
 public class Config {
+    public static final String CLIENT_ID = "6879e13702256246d424c35face10dad";
+    public static final String API_URL = "https://api.soundcloud.com";
 }
---
+++ SoundCPlayer/app/src/main/java/com/dismas/imaya/soundcplayer/Track.java
@@ -0,0 +1,7 @@
+package com.dismas.imaya.soundcplayer;
+
+/**
+ * Created by imaya on 3/12/16.
+ */
+public class Track {
+}
