5f2d9e0b23b3f0343ec7624e954eb5f0e0b40206
ImayaDismas <imayadismas@gmail.com>
2016-03-13 02:42:46 +0300

    Make a copy of the SoundCplayer

---
+++ Sound/app/src/androidTest/java/com/dismas/imaya/soundcplayer/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.dismas.imaya.soundcplayer;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
---
+++ Sound/app/src/main/java/com/dismas/imaya/soundcplayer/Track.java
@@ -0,0 +1,37 @@
+package com.dismas.imaya.soundcplayer;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by imaya on 3/12/16.
+ */
+public class Track {
+
+    @SerializedName("title")
+    private String mTitle;
+
+    @SerializedName("id")
+    private int mID;
+
+    @SerializedName("stream_url")
+    private String mStreamURL = "http://api.soundcloud.com/tracks/13158665.json?client_id=6879e13702256246d424c35face10dad";
+
+    @SerializedName("artwork_url")
+    private String mArtworkURL = "http://api.soundcloud.com/tracks/13158665.json?client_id=6879e13702256246d424c35face10dad";
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public int getID() {
+        return mID;
+    }
+
+    public String getStreamURL() {
+        return mStreamURL;
+    }
+
+    public String getArtworkURL() {
+        return mArtworkURL;
+    }
+}
