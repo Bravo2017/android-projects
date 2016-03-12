1b0cf6bb0a44a6d46ee86d3c115573a00924f6fc
ImayaDismas <imayadismas@gmail.com>
2016-03-12 15:24:10 +0300

    Main and SCService complete

--- SoundCPlayer/app/src/main/java/com/dismas/imaya/soundcplayer/Main.java
+++ SoundCPlayer/app/src/main/java/com/dismas/imaya/soundcplayer/Main.java
@@ -2,12 +2,38 @@ package com.dismas.imaya.soundcplayer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+import retrofit.Callback;
+import retrofit.RestAdapter;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
 
 public class Main extends AppCompatActivity {
+
+    private static final String TAG = "Main";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        RestAdapter restAdapter = new RestAdapter.Builder().setEndpoint(Config.API_URL).build();
+        SCService scService = restAdapter.create(SCService.class);
+        scService.getRecentTracks(new SimpleDateFormat("yyyy-MM-dd hh:mm:ss").format(new Date()), new Callback<List<Track>>() {
+            @Override
+            public void success(List<Track> tracks, Response response) {
+                Log.d(TAG, "First track title: " + tracks.get(0).getTitle());
+            }
+
+            @Override
+            public void failure(RetrofitError error) {
+                Log.d(TAG, "Error: " + error);
+            }
+        });
     }
 }
