24272958776d3ec51088b9c3ed870292d7f16c71
ImayaDismas <imayadismas@gmail.com>
2016-03-14 20:16:32 +0300

    setting the main layout for content

--- GridView/app/src/main/java/com/dismas/imaya/gridview/Main.java
+++ GridView/app/src/main/java/com/dismas/imaya/gridview/Main.java
@@ -2,6 +2,10 @@ package com.dismas.imaya.gridview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
 
 public class Main extends AppCompatActivity {
 
@@ -9,5 +13,16 @@ public class Main extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        GridView gridview = (GridView) findViewById(R.id.gridview);
+        gridview.setAdapter(new ImageAdapter(this));
+
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+                Toast.makeText(Main.this, "" + position,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
