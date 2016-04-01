92fb92c493a68ee9a7e1779a99e395fd0e357536
ImayaDismas <imayadismas@gmail.com>
2016-04-01 13:36:26 +0300

    The camera complete

--- Bits/app/src/main/java/com/dismas/imaya/bits/AllBits.java
+++ Bits/app/src/main/java/com/dismas/imaya/bits/AllBits.java
@@ -1,16 +1,40 @@
 package com.dismas.imaya.bits;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by imaya on 4/1/16.
  */
 public class AllBits extends AppCompatActivity {
-
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_allbits);
+        addListenerOnButton();
+    }
+    public void addListenerOnButton() {
+
+        final Context context = this;
+
+        button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Intent intent = new Intent(context, MainCamera.class);
+                startActivity(intent);
+
+            }
+
+        });
+
     }
 }
--- Bits/app/src/main/java/com/dismas/imaya/bits/MainCamera.java
+++ Bits/app/src/main/java/com/dismas/imaya/bits/MainCamera.java
@@ -30,7 +30,7 @@ public class MainCamera extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_maincamera);
 
         Display display = getWindowManager().getDefaultDisplay();
         mSize = new Point();
