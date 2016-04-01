c09abe05ce20363ec5bbb3158088cca0a0417f0a
ImayaDismas <imayadismas@gmail.com>
2016-04-01 14:12:42 +0300

    Edited the all bits activity

--- Bits/app/src/main/java/com/dismas/imaya/bits/AllBits.java
+++ Bits/app/src/main/java/com/dismas/imaya/bits/AllBits.java
@@ -1,40 +1,115 @@
 package com.dismas.imaya.bits;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Point;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Display;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+
+import com.dismas.imaya.squarecamera.CameraActivity;
+import com.dismas.imaya.squarecamera.ImageUtility;
 
 /**
  * Created by imaya on 4/1/16.
  */
 public class AllBits extends AppCompatActivity {
-    Button button;
+    private static final int REQUEST_CAMERA = 0;
+    private static final int REQUEST_CAMERA_PERMISSION = 1;
+    private Point mSize;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_allbits);
-        addListenerOnButton();
+
+        Display display = getWindowManager().getDefaultDisplay();
+        mSize = new Point();
+        display.getSize(mSize);
     }
-    public void addListenerOnButton() {
 
-        final Context context = this;
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode != RESULT_OK) return;
 
-        button = (Button) findViewById(R.id.button);
+        if (requestCode == REQUEST_CAMERA) {
+            Uri photoUri = data.getData();
+            // Get the bitmap in according to the width of the device
+            Bitmap bitmap = ImageUtility.decodeSampledBitmapFromPath(photoUri.getPath(), mSize.x, mSize.x);
+            ((ImageView) findViewById(R.id.image)).setImageBitmap(bitmap);
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
 
-        button.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View arg0) {
-
-                Intent intent = new Intent(context, MainCamera.class);
-                startActivity(intent);
-
+    public void requestForCameraPermission(View view) {
+        final String permission = Manifest.permission.CAMERA;
+        if (ContextCompat.checkSelfPermission(AllBits.this, permission)
+                != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(AllBits.this, permission)) {
+                showPermissionRationaleDialog("Test", permission);
+            } else {
+                requestForPermission(permission);
             }
+        } else {
+            launch();
+        }
+    }
 
-        });
+    private void showPermissionRationaleDialog(final String message, final String permission) {
+        new AlertDialog.Builder(AllBits.this)
+                .setMessage(message)
+                .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        AllBits.this.requestForPermission(permission);
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
 
+                    }
+                })
+                .create()
+                .show();
+    }
+
+    private void requestForPermission(final String permission) {
+        ActivityCompat.requestPermissions(AllBits.this, new String[]{permission}, REQUEST_CAMERA_PERMISSION);
+    }
+
+    private void launch() {
+        Intent startCustomCameraIntent = new Intent(this, CameraActivity.class);
+        startActivityForResult(startCustomCameraIntent, REQUEST_CAMERA);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CAMERA_PERMISSION:
+                final int numOfRequest = grantResults.length;
+                final boolean isGranted = numOfRequest == 1
+                        && PackageManager.PERMISSION_GRANTED == grantResults[numOfRequest - 1];
+                if (isGranted) {
+                    launch();
+                }
+                break;
+
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
     }
 }
+
