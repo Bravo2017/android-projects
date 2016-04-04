8f8735b7d970cde7763c5db827eaee4013aa160b
ImayaDismas <imayadismas@gmail.com>
2016-04-04 19:18:34 +0300

    Created a volume percent view

---
+++ Bits/app/src/main/java/com/dismas/imaya/bits/VolumePercentView.java
@@ -0,0 +1,56 @@
+package com.dismas.imaya.bits;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+/**
+ * Created by imaya on 4/4/16.
+ */
+public class VolumePercentView extends TextView {
+    private static final float MAX_POSSIBLE_AMPLITUDE = 32768F;
+    private static final long IDLE_REFRESH_INTERVAL = 1000;
+    private static final long LIVE_REFRESH_INTERVAL = 100;
+    private int volumePercentage = 0;
+    private InstrumentedRecorder recorder;
+
+    public VolumePercentView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        setPercentageText();
+    }
+
+    public VolumePercentView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setPercentageText();
+    }
+
+    public VolumePercentView(Context context) {
+        super(context);
+        setPercentageText();
+    }
+
+    public void setRecorder(InstrumentedRecorder recorder) {
+        this.recorder = recorder;
+    }
+
+    private void setPercentageText() {
+        if (recorder != null && recorder.getState() == InstrumentedRecorder.RECORDING_STATE) {
+            float volumePercentageDouble = (recorder.getMaxAmplitude() / MAX_POSSIBLE_AMPLITUDE) * 100;
+            volumePercentage = Math.round(volumePercentageDouble);
+        }
+        setText(volumePercentage + "%");
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        if (recorder != null && recorder.getState() == InstrumentedRecorder.RECORDING_STATE) {
+            setPercentageText();
+            postInvalidateDelayed(LIVE_REFRESH_INTERVAL);
+        }
+        else {
+            postInvalidateDelayed(IDLE_REFRESH_INTERVAL);
+        }
+        super.onDraw(canvas);
+    }
+}
