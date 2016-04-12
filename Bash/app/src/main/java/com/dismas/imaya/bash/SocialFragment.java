0ed60b1d1181b31519bb87d50abb8bc7fba8f63d
ImayaDismas <imayadismas@gmail.com>
2016-04-13 01:23:17 +0300

    Added content to the activities created

--- Bash/app/src/main/java/com/dismas/imaya/bash/SocialFragment.java
+++ Bash/app/src/main/java/com/dismas/imaya/bash/SocialFragment.java
@@ -1,9 +1,19 @@
 package com.dismas.imaya.bash;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by imaya on 4/13/16.
  */
 public class SocialFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.primary_layout,null);
+    }
 }
