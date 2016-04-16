ebcb8044308a7863f022e11b70d1e7f7ca25ab1b
ImayaDismas <imayadismas@gmail.com>
2016-04-16 12:39:20 +0300

    Edited the main activity

--- Retrofit-trial/app/src/main/java/com/dismas/imaya/retrofit_trial/MainActivity.java
+++ Retrofit-trial/app/src/main/java/com/dismas/imaya/retrofit_trial/MainActivity.java
@@ -3,8 +3,11 @@ package com.dismas.imaya.retrofit_trial;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
+import android.view.LayoutInflater;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.Bind;
@@ -16,37 +19,39 @@ import retrofit.client.Response;
 
 public class MainActivity extends AppCompatActivity {
 
+    String API = "https://api.github.com"; // BASE URL
+    LayoutInflater inflater;
+    List<GitModel> placeholderModel;
+    ListViewAdapter listAdapter;
+    @Bind(R.id.listView)
+    ListView list;
     @Bind(R.id.texv)
     TextView tv;
-    String API = "https://api.github.com";                         //BASE URL
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         ButterKnife.bind(this);
-        RestAdapter restAdapter = new RestAdapter.Builder()
-                .setEndpoint(API).build();                                        //create an adapter for retrofit with base url
+        inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
 
-        GitApi git = restAdapter.create(GitApi.class);                            //creating a service for adapter with our GET class
+        placeholderModel = new ArrayList<>();
+        listAdapter = new ListViewAdapter(inflater, placeholderModel);
+
+        list.setAdapter(listAdapter);
+        RestAdapter restAdapter = new RestAdapter.Builder()
+                .setEndpoint(API).build();  //create an adapter for retrofit with base url
+
+        GitApi git = restAdapter.create(GitApi.class);  //creating a service for adapter with our GET class
 
         git.getFeed(new Callback<List<GitModel>>() {
             @Override
             public void failure(RetrofitError error) {
                 tv.setText(error.getMessage());
             }
-
             @Override
-            public void success(List<GitModel> GitModel, Response response) {
-                //we get json object from github server to our POJO or model class
-                tv.setMovementMethod(new ScrollingMovementMethod());
-                for (int i = 0; i < GitModel.size(); i++) {
-                    tv.append("" + GitModel.get(i).getName() + "\n id: "
-                            + GitModel.get(i).getId() + "\n full name: "
-                            + GitModel.get(i).getfullName() + "\n Avatar Url: "
-                            + GitModel.get(i).getOwner().getAvatar_url() + "\n\n\n");
-                }
+            public void success(final List<GitModel> gitmodel, Response response) {
+                listAdapter.setGitmodel(gitmodel);
             }
         });
     }
