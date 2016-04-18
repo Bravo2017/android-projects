acfe0d5c12605364e2a0c4ec3fba114c27870fb2
ImayaDismas <imayadismas@gmail.com>
2016-04-18 09:59:46 +0300

    Added a web view layout and activity

---
+++ Retrofit-trial/app/src/main/java/com/dismas/imaya/retrofit_trial/SingleListItem.java
@@ -0,0 +1,29 @@
+package com.dismas.imaya.retrofit_trial;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+/**
+ * Created by imaya on 4/18/16.
+ */
+public class SingleListItem extends Activity {
+    @Bind(R.id.webview)
+    WebView browser;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.single_list_item);
+        ButterKnife.bind(this);
+
+        Intent i = getIntent();
+        String url = i.getStringExtra("url");
+        browser.loadUrl(url);
+    }
+}
+ 
