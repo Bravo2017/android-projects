69092f6f72223c042f7d1c3274b7c6987e3c3c17
ImayaDismas <imayadismas@gmail.com>
2016-04-19 22:19:41 +0300

    Added a github model

---
+++ evryBit/app/src/main/java/com/dismas/imaya/evrybit/GitModel.java
@@ -0,0 +1,44 @@
+package com.dismas.imaya.evrybit;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by imaya on 4/19/16.
+ */
+public class GitModel {
+    @Expose
+    private String login;
+    @Expose
+    private Integer id;
+    @Expose
+    private String name;
+    @SerializedName("full_name")
+    @Expose
+    private String fullName;
+
+    public String getLogin() {
+        return login;
+    }
+    public void setLogin(String login) {
+        this.login = login;
+    }
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getfullName() {
+        return fullName;
+    }
+    public void setfullName(String fullName) {
+        this.fullName = fullName;
+    }
+}
