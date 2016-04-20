9b535599b431187f8dab7b371ac936f474e07cc8
ImayaDismas <imayadismas@gmail.com>
2016-04-21 01:49:25 +0300

    A complete project with different json files

---
+++ JsonParserRetrofit/app/src/main/java/com/dismas/imaya/jsonparserretrofit/RestClient.java
@@ -0,0 +1,28 @@
+package com.dismas.imaya.jsonparserretrofit;
+
+/**
+ * Created by imaya on 4/21/16.
+ */
+public class RestClient {
+    //You need to change the IP if you testing environment is not local machine
+    //or you may have different from what we have here
+    private static final String URL = "http://instinctcoder.com/wp-content/uploads/2015/08/";
+    private retrofit.RestAdapter restAdapter;
+    private StudentService studentService;
+
+    public RestClient()
+    {
+
+        restAdapter = new retrofit.RestAdapter.Builder()
+                .setEndpoint(URL)
+                .setLogLevel(retrofit.RestAdapter.LogLevel.FULL)
+                .build();
+
+        studentService = restAdapter.create(StudentService.class);
+    }
+
+    public StudentService getService()
+    {
+        return studentService;
+    }
+}
