6607d17560f67efb7ae232b9c7d876c7b95da255
ImayaDismas <imayadismas@gmail.com>
2016-05-04 20:17:22 +0300

    Edited the splash and app intro

---
+++ Bash/app/src/main/java/com/dismas/imaya/bash/SplashScreenAlways.java
@@ -0,0 +1,40 @@
+package com.dismas.imaya.bash;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+/**
+ * Created by imaya on 5/4/16.
+ */
+public class SplashScreenAlways extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(3000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent intent = new Intent(SplashScreenAlways.this,MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        finish();
+    }
+
+}
