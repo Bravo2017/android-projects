1c266f6c7fa67b11e83a091b006409430a176f6e
ImayaDismas <imayadismas@gmail.com>
2016-05-10 00:15:48 +0300

    Added a youtube parsing helper

---
+++ Bash/app/src/main/java/com/dismas/imaya/bash/services/youtube/YoutubeParsingHelper.java
@@ -0,0 +1,47 @@
+package com.dismas.imaya.bash.services.youtube;
+
+import com.dismas.imaya.bash.extractor.ParsingException;
+
+/**
+ * Created by imaya on 5/10/16.
+ */
+public class YoutubeParsingHelper {
+    private YoutubeParsingHelper() {
+    }
+
+    public static int parseDurationString(String input)
+            throws ParsingException, NumberFormatException {
+        String[] splitInput = input.split(":");
+        String days = "0";
+        String hours = "0";
+        String minutes = "0";
+        String seconds;
+
+        switch(splitInput.length) {
+            case 4:
+                days = splitInput[0];
+                hours = splitInput[1];
+                minutes = splitInput[2];
+                seconds = splitInput[3];
+                break;
+            case 3:
+                hours = splitInput[0];
+                minutes = splitInput[1];
+                seconds = splitInput[2];
+                break;
+            case 2:
+                minutes = splitInput[0];
+                seconds = splitInput[1];
+                break;
+            case 1:
+                seconds = splitInput[0];
+                break;
+            default:
+                throw new ParsingException("Error duration string with unknown format: " + input);
+        }
+        return ((((Integer.parseInt(days) * 24)
+                + Integer.parseInt(hours) * 60)
+                + Integer.parseInt(minutes)) * 60)
+                + Integer.parseInt(seconds);
+    }
+}
