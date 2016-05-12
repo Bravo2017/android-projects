ea12cb3ca4ec51fe085910fa38d8caa515898d31
ImayaDismas <imayadismas@gmail.com>
2016-05-12 03:41:25 +0300

    Material design card and recycler view complete

---
+++ MaterialCards/app/src/main/java/com/dismas/imaya/materialcards/ItemObject.java
@@ -0,0 +1,30 @@
+package com.dismas.imaya.materialcards;
+
+/**
+ * Created by imaya on 5/12/16.
+ */
+public class ItemObject {
+    private String name;
+    private int photo;
+
+    public ItemObject(String name, int photo) {
+        this.name = name;
+        this.photo = photo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(int photo) {
+        this.photo = photo;
+    }
+}
