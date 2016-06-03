0d60bb85e427b2a1cb0ed4f870b1b40bbf953d4d
ImayaDismas <imayadismas@gmail.com>
2016-06-04 01:59:50 +0300

    Added a background color to detail using palette

--- Travel_list/app/src/main/java/bhouse/travellist_starterproject/DetailActivity.java
+++ Travel_list/app/src/main/java/bhouse/travellist_starterproject/DetailActivity.java
@@ -5,8 +5,10 @@ import android.app.Activity;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.v7.graphics.Palette;
 import android.view.View;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
@@ -64,6 +66,7 @@ public class DetailActivity extends Activity implements View.OnClickListener {
     loadPlace();
     windowTransition();
     getPhoto();
+
   }
 
   private void setUpAdapter() {
@@ -87,13 +90,18 @@ public class DetailActivity extends Activity implements View.OnClickListener {
 
   private void getPhoto() {
     Bitmap photo = BitmapFactory.decodeResource(getResources(), mPlace.getImageResourceId(this));
+      colorize(photo);
   }
 
   private void colorize(Bitmap photo) {
+    Palette mPalette = Palette.generate(photo);
+    applyPalette(mPalette);
   }
 
-  private void applyPalette() {
-
+  private void applyPalette(Palette mPalette) {
+    getWindow().setBackgroundDrawable(new ColorDrawable(mPalette.getDarkMutedColor(defaultColor)));
+    mTitleHolder.setBackgroundColor(mPalette.getMutedColor(defaultColor));
+    mRevealView.setBackgroundColor(mPalette.getLightVibrantColor(defaultColor));
   }
 
   @Override
