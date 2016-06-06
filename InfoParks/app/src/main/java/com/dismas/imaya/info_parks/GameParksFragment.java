9d8895d614428a68b1f0accb0e2cf08f213343cc
ImayaDismas <imayadismas@gmail.com>
2016-06-06 23:01:46 +0300

    Added the contents of the game parks in kenya

--- InfoParks/app/src/main/java/com/dismas/imaya/info_parks/GameParksFragment.java
+++ InfoParks/app/src/main/java/com/dismas/imaya/info_parks/GameParksFragment.java
@@ -1,9 +1,54 @@
 package com.dismas.imaya.info_parks;
 
-import android.support.v4.app.Fragment;
+import android.app.Activity;
+import android.app.Fragment;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
 
 /**
  * Created by imaya on 5/27/16.
  */
 public class GameParksFragment extends Fragment {
+    private Menu menu;
+    private boolean isListView;
+    private RecyclerView mRecyclerView;
+    private StaggeredGridLayoutManager mStaggeredLayoutManager;
+    private TravelListAdapter mAdapter;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.activity_gameparksfragment,null);
+
+        isListView = true;
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.list);
+        mStaggeredLayoutManager = new StaggeredGridLayoutManager(1, StaggeredGridLayoutManager.VERTICAL);
+        mRecyclerView.setLayoutManager(mStaggeredLayoutManager);
+
+        mAdapter = new TravelListAdapter(this);
+        mRecyclerView.setAdapter(mAdapter);
+
+        TravelListAdapter.OnItemClickListener onItemClickListener = new TravelListAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(View v, int position) {
+                Toast.makeText(MainActivity.this, "Clicked " + position, Toast.LENGTH_SHORT).show();
+
+                Intent intent = new Intent(MainActivity.this, DetailActivity.class);
+                intent.putExtra(DetailActivity.EXTRA_PARAM_ID, position);
+                startActivity(intent);
+            }
+        };
+
+        mAdapter.setOnItemClickListener(onItemClickListener);
+    }
 }
