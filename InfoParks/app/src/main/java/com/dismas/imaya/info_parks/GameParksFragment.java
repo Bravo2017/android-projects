57dba937f8fde85e83ba2dd2d17a0a3623e45532
ImayaDismas <imayadismas@gmail.com>
2016-06-08 02:04:16 +0300

    removed unnecessary images of no use

--- InfoParks/app/src/main/java/com/dismas/imaya/info_parks/GameParksFragment.java
+++ InfoParks/app/src/main/java/com/dismas/imaya/info_parks/GameParksFragment.java
@@ -1,5 +1,6 @@
 package com.dismas.imaya.info_parks;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -39,7 +40,7 @@ public class GameParksFragment extends Fragment {
         TravelListAdapter.OnItemClickListener onItemClickListener = new TravelListAdapter.OnItemClickListener() {
             @Override
             public void onItemClick(View v, int position) {
-                Toast.makeText(getActivity(), "Clicked " + position, Toast.LENGTH_SHORT).show();
+                //Toast.makeText(getActivity(), "Clicked " + position, Toast.LENGTH_SHORT).show();
 
                 if(position == 0)
                 {
@@ -49,127 +50,133 @@ public class GameParksFragment extends Fragment {
                 }
                 else if(position == 1)
                 {
-                    Intent intent = new Intent(getActivity(), SplashScreenAlways.class);
+                    Intent intent = new Intent(getActivity(), AmboseliActivity.class);
                     intent.putExtra(DetailActivity.EXTRA_PARAM_ID, position);
                     startActivity(intent);
                 }
                 else if(position == 2)
                 {
-                    Intent intent = new Intent(getActivity(), SplashScreenAlways.class);
+                    Intent intent = new Intent(getActivity(), ArabukoActivity.class);
                     intent.putExtra(DetailActivity.EXTRA_PARAM_ID, position);
                     startActivity(intent);
                 }
                 else if(position == 3)
                 {
-                    Intent intent = new Intent(getActivity(), SplashScreenAlways.class);
+                    Intent intent = new Intent(getActivity(), BogoriaActivity.class);
                     intent.putExtra(DetailActivity.EXTRA_PARAM_ID, position);
                     startActivity(intent);
                 }
                 else if(position == 4)
                 {
-                    Intent intent = new Intent(getActivity(), SplashScreenAlways.class);
+                    Intent intent = new Intent(getActivity(), BuffaloActivity.class);
                     intent.putExtra(DetailActivity.EXTRA_PARAM_ID, position);
                     startActivity(intent);
                 }
                 else if(position == 5)
                 {
-                    Intent intent = new Intent(getActivity(), SplashScreenAlways.class);
+                    Intent intent = new Intent(getActivity(), HellsActivity.class);
                     intent.putExtra(DetailActivity.EXTRA_PARAM_ID, position);
                     startActivity(intent);
                 }
                 else if(position == 6)
                 {
-                    Intent intent = new Intent(getActivity(), SplashScreenAlways.class);
+                    Intent intent = new Intent(getActivity(), KakamegaActivity.class);
                     intent.putExtra(DetailActivity.EXTRA_PARAM_ID, position);
                     startActivity(intent);
                 }
                 else if(position == 7)
                 {
-                    Intent intent = new Intent(getActivity(), SplashScreenAlways.class);
+                    Intent intent = new Intent(getActivity(), LewaActivity.class);
                     intent.putExtra(DetailActivity.EXTRA_PARAM_ID, position);
                     startActivity(intent);
                 }
                 else if(position == 8)
                 {
-                    Intent intent = new Intent(getActivity(), SplashScreenAlways.class);
+                    Intent intent = new Intent(getActivity(), MaasaiActivity.class);
                     intent.putExtra(DetailActivity.EXTRA_PARAM_ID, position);
                     startActivity(intent);
                 }
                 else if(position == 9)
                 {
-                    Intent intent = new Intent(getActivity(), SplashScreenAlways.class);
+                    Intent intent = new Intent(getActivity(), MarsabitActivity.class);
                     intent.putExtra(DetailActivity.EXTRA_PARAM_ID, position);
                     startActivity(intent);
                 }
                 else if(position == 10)
                 {
-                    Intent intent = new Intent(getActivity(), SplashScreenAlways.class);
+                    Intent intent = new Intent(getActivity(), MeruActivity.class);
                     intent.putExtra(DetailActivity.EXTRA_PARAM_ID, position);
                     startActivity(intent);
                 }
                 else if(position == 11)
                 {
-                    Intent intent = new Intent(getActivity(), SplashScreenAlways.class);
+                    Intent intent = new Intent(getActivity(), MombasaActivity.class);
                     intent.putExtra(DetailActivity.EXTRA_PARAM_ID, position);
                     startActivity(intent);
                 }
                 else if(position == 12)
                 {
-                    Intent intent = new Intent(getActivity(), SplashScreenAlways.class);
+                    Intent intent = new Intent(getActivity(), MtkenyaActivity.class);
                     intent.putExtra(DetailActivity.EXTRA_PARAM_ID, position);
                     startActivity(intent);
                 }
                 else if(position == 13)
                 {
-                    Intent intent = new Intent(getActivity(), SplashScreenAlways.class);
+                    Intent intent = new Intent(getActivity(), MtlongonotActivity.class);
                     intent.putExtra(DetailActivity.EXTRA_PARAM_ID, position);
                     startActivity(intent);
                 }
                 else if(position == 14)
                 {
-                    Intent intent = new Intent(getActivity(), SplashScreenAlways.class);
+                    Intent intent = new Intent(getActivity(), NaivashaActivity.class);
                     intent.putExtra(DetailActivity.EXTRA_PARAM_ID, position);
                     startActivity(intent);
                 }
                 else if(position == 15)
                 {
-                    Intent intent = new Intent(getActivity(), SplashScreenAlways.class);
+                    Intent intent = new Intent(getActivity(), NakuruActivity.class);
                     intent.putExtra(DetailActivity.EXTRA_PARAM_ID, position);
                     startActivity(intent);
                 }
                 else if(position == 16)
                 {
-                    Intent intent = new Intent(getActivity(), SplashScreenAlways.class);
+                    Intent intent = new Intent(getActivity(), SamburuActivity.class);
                     intent.putExtra(DetailActivity.EXTRA_PARAM_ID, position);
                     startActivity(intent);
                 }
                 else if(position == 17)
                 {
-                    Intent intent = new Intent(getActivity(), SplashScreenAlways.class);
+                    Intent intent = new Intent(getActivity(), ShabaActivity.class);
                     intent.putExtra(DetailActivity.EXTRA_PARAM_ID, position);
                     startActivity(intent);
                 }
                 else if(position == 18)
                 {
-                    Intent intent = new Intent(getActivity(), SplashScreenAlways.class);
+                    Intent intent = new Intent(getActivity(), ShimbaActivity.class);
                     intent.putExtra(DetailActivity.EXTRA_PARAM_ID, position);
                     startActivity(intent);
                 }
                 else if(position == 19)
                 {
-                    Intent intent = new Intent(getActivity(), SplashScreenAlways.class);
+                    Intent intent = new Intent(getActivity(), TaitaActivity.class);
                     intent.putExtra(DetailActivity.EXTRA_PARAM_ID, position);
                     startActivity(intent);
                 }
                 else if(position == 20)
                 {
-                    Intent intent = new Intent(getActivity(), SplashScreenAlways.class);
+                    Intent intent = new Intent(getActivity(), TsavoEastActivity.class);
                     intent.putExtra(DetailActivity.EXTRA_PARAM_ID, position);
                     startActivity(intent);
                 }
                 else if(position == 21)
                 {
-                    Intent intent = new Intent(getActivity(), SplashScreenAlways.class);
+                    Intent intent = new Intent(getActivity(), TsavoWestActivity.class);
+                    intent.putExtra(DetailActivity.EXTRA_PARAM_ID, position);
+                    startActivity(intent);
+                }
+                else if(position == 22)
+                {
+                    Intent intent = new Intent(getActivity(), WatamuActivity.class);
                     intent.putExtra(DetailActivity.EXTRA_PARAM_ID, position);
                     startActivity(intent);
                 }
