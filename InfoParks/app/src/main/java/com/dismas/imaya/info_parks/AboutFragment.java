2477a974bde6a651ecb610824725cc1f318f032b
ImayaDismas <imayadismas@gmail.com>
2016-06-13 23:40:33 +0300

    Added the contents of the about fragment

--- InfoParks/app/src/main/java/com/dismas/imaya/info_parks/AboutFragment.java
+++ InfoParks/app/src/main/java/com/dismas/imaya/info_parks/AboutFragment.java
@@ -1,9 +1,19 @@
 package com.dismas.imaya.info_parks;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by imaya on 6/13/16.
  */
 public class AboutFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.about_layout,null);
+    }
 }
