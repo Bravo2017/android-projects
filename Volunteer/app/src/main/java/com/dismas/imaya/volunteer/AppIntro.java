b6c7263bfdd7792e8dc2b4f615fd43638416e221
ImayaDismas <imayadismas@gmail.com>
2016-06-17 23:20:47 +0300

    Added an app intro

---
+++ Volunteer/app/src/main/java/com/dismas/imaya/volunteer/AppIntro.java
@@ -0,0 +1,16 @@
+package com.dismas.imaya.volunteer;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by imaya on 6/17/16.
+ */
+public class AppIntro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_appintro);
+    }
+}
