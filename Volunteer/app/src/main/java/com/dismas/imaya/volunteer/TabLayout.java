6ff68bd6f39a4a1fb9e61c4515d01637934dee74
ImayaDismas <imayadismas@gmail.com>
2016-06-19 23:17:19 +0300

    Added a tab layout view

---
+++ Volunteer/app/src/main/java/com/dismas/imaya/volunteer/TabLayout.java
@@ -0,0 +1,16 @@
+package com.dismas.imaya.volunteer;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by imaya on 6/19/16.
+ */
+public class TabLayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tab_layout);
+    }
+}
