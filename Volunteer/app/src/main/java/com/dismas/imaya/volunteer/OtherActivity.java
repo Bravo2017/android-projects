de4a359f88844c16753507b5e03542cebe9f5d1b
ImayaDismas <imayadismas@gmail.com>
2016-06-20 23:55:42 +0300

    Added a new activity and layout

---
+++ Volunteer/app/src/main/java/com/dismas/imaya/volunteer/OtherActivity.java
@@ -0,0 +1,16 @@
+package com.dismas.imaya.volunteer;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by imaya on 6/20/16.
+ */
+public class OtherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_other);
+    }
+}
