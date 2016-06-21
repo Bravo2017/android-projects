4487847940c065e1793c4de74133d23a00ca661e
ImayaDismas <imayadismas@gmail.com>
2016-06-21 23:48:00 +0300

    Created a new activity and layout

---
+++ Volunteer/app/src/main/java/com/dismas/imaya/volunteer/NewActivity.java
@@ -0,0 +1,16 @@
+package com.dismas.imaya.volunteer;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by imaya on 6/21/16.
+ */
+public class NewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new);
+    }
+}
