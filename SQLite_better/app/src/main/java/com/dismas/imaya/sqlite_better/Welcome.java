517a1baa6996279d311fe53667bc93842963ec51
ImayaDismas <imayadismas@gmail.com>
2016-07-05 23:57:30 +0300

    All activities complete

--- SQLite_better/app/src/main/java/com/dismas/imaya/sqlite_better/Welcome.java
+++ SQLite_better/app/src/main/java/com/dismas/imaya/sqlite_better/Welcome.java
@@ -1,7 +1,16 @@
 package com.dismas.imaya.sqlite_better;
 
+import android.app.Activity;
+import android.os.Bundle;
+
 /**
  * Created by imaya on 7/5/16.
  */
-public class Welcome {
+public class Welcome extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.welcome);
+    }
 }
