1c33f8b51eadd4170fac6840c28e0cb4e6beb639
ImayaDismas <imayadismas@gmail.com>
2016-07-12 03:29:50 +0300

    Created all the activities

--- ActionBar_Fading/app/src/main/java/com/dismas/imaya/actionbar_fading/ScrollViewX.java
+++ ActionBar_Fading/app/src/main/java/com/dismas/imaya/actionbar_fading/ScrollViewX.java
@@ -1,7 +1,36 @@
 package com.dismas.imaya.actionbar_fading;
 
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ScrollView;
+
 /**
  * Created by imaya on 7/12/16.
  */
-public class ScrollViewX {
+public class ScrollViewX extends ScrollView {
+
+    private OnScrollViewListener mOnScrollViewListener;
+
+    public ScrollViewX(Context context) {
+        super(context);
+    }
+    public ScrollViewX(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+    public ScrollViewX(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    public interface OnScrollViewListener {
+        void onScrollChanged( ScrollViewX v, int l, int t, int oldl, int oldt );
+    }
+
+    public void setOnScrollViewListener(OnScrollViewListener l) {
+        this.mOnScrollViewListener = l;
+    }
+
+    protected void onScrollChanged(int l, int t, int oldl, int oldt) {
+        mOnScrollViewListener.onScrollChanged( this, l, t, oldl, oldt );
+        super.onScrollChanged( l, t, oldl, oldt );
+    }
 }
