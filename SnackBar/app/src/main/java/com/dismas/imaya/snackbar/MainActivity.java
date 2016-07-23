b79ae350854f94d8cf447e9664f79df7c3993f5a
ImayaDismas <imayadismas@gmail.com>
2016-07-23 16:51:57 +0300

    Edited the main activity and layout

--- SnackBar/app/src/main/java/com/dismas/imaya/snackbar/MainActivity.java
+++ SnackBar/app/src/main/java/com/dismas/imaya/snackbar/MainActivity.java
@@ -1,7 +1,11 @@
 package com.dismas.imaya.snackbar;
 
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final CoordinatorLayout Clayout = (CoordinatorLayout)findViewById(R.id.snackbarlocation);
+
+        FloatingActionButton FabButton = null;
+
+        FabButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Snackbar.make(Clayout, "This snack bar located at top", Snackbar.LENGTH_SHORT).show();
+            }
+        });
+
+        View.OnClickListener snackbarClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        };
     }
+
+
 }
