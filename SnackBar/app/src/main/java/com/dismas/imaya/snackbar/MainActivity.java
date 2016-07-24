b15713b99eb99319dc999075465c562cf4935b51
ImayaDismas <imayadismas@gmail.com>
2016-07-24 17:01:41 +0300

    Edited the main activity for floating action bar to work

--- SnackBar/app/src/main/java/com/dismas/imaya/snackbar/MainActivity.java
+++ SnackBar/app/src/main/java/com/dismas/imaya/snackbar/MainActivity.java
@@ -6,17 +6,16 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         final CoordinatorLayout Clayout = (CoordinatorLayout)findViewById(R.id.snackbarlocation);
-
-        FloatingActionButton FabButton = null;
+        final FloatingActionButton FabButton = (FloatingActionButton) findViewById(R.id.add_fab_button);
 
         FabButton.setOnClickListener(new View.OnClickListener() {
             @Override
