d5f4cafecb531db8441899e8b448f57e123fdee8
ImayaDismas <imayadismas@gmail.com>
2016-09-18 02:26:17 +0300

    The registration and login app complete

---
+++ CompleteLogin/app/src/main/java/com/dismas/imaya/completelogin/app/AppConfig.java
@@ -0,0 +1,12 @@
+package com.dismas.imaya.completelogin.app;
+
+/**
+ * Created by imaya on 9/18/16.
+ */
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "http://192.168.43.238/recommender_android_api/login.php";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://1192.168.43.238/recommender_android_api/register.php";
+}
