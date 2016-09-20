caf2e6dad0d48bf3e76ba3601ad00570360d4370
ImayaDismas <imayadismas@gmail.com>
2016-09-21 00:58:11 +0300

    Created uber example and the final year project

--- CompleteLogin/app/src/main/java/com/dismas/imaya/completelogin/app/AppConfig.java
+++ CompleteLogin/app/src/main/java/com/dismas/imaya/completelogin/app/AppConfig.java
@@ -8,5 +8,5 @@ public class AppConfig {
     public static String URL_LOGIN = "http://192.168.43.238/recommender_android_api/login.php";
 
     // Server user register url
-    public static String URL_REGISTER = "http://1192.168.43.238/recommender_android_api/register.php";
+    public static String URL_REGISTER = "http://192.168.43.238/recommender_android_api/register.php";
 }
