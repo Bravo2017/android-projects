1879f90645a87de49b0c144e4f855874c70fcc27
ImayaDismas <imayadismas@gmail.com>
2016-11-03 03:00:43 +0300

    the code for the feeds working correctly

---
+++ Feed/app/src/main/java/com/dismas/imaya/feed/Config.java
@@ -0,0 +1,16 @@
+package com.dismas.imaya.feed;
+
+/**
+ * Created by imaya on 11/3/16.
+ */
+
+public class Config {
+    //Data URL
+    public static final String DATA_URL = "http://192.168.43.238/feed/feed.php?page=";
+
+    //JSON TAGS http://imaya-dismas/feed/feed.php?page=
+    public static final String TAG_IMAGE_URL = "image_url";
+    public static final String TAG_NAME = "name";
+    public static final String TAG_PUBLISHER = "publishers";
+}
+
---
+++ Feed/app/src/main/java/com/dismas/imaya/feed/SuperHero.java
@@ -0,0 +1,37 @@
+package com.dismas.imaya.feed;
+
+/**
+ * Created by imaya on 11/3/16.
+ */
+
+public class SuperHero {
+    //Data Variables
+    private String imageUrl;
+    private String name;
+    private String publishers;
+
+    //Getters and Setters
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPublisher() {
+        return publishers;
+    }
+
+    public void setPublisher(String publishers) {
+        this.publishers = publishers;
+    }
+}
